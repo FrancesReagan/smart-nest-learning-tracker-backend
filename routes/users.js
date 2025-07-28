c0fc7c987f0f9d1418f4f1025d515dba001f51f9
Frances Reagan James <adtgwen@gmail.com>
2025-07-28 13:41:21 -0400

    starting user.js route working on create new user route POST /api/users/register endpoint creation

--- routes/users.js
+++ routes/users.js
@@ -2,3 +2,23 @@
 import express from "express";
 import User from "../models/User.js";
 import { signToken, authMiddleware } from "../utils/auth.js";
+
+const router = express.Router();
+
+// POST /api/users/register - Create a new user//
+router.post("/register", async (req, res) => {
+
+try {
+  const user = await User.create(req.body);
+  const token = signToken(user);
+  res.status(201).json({token, user });
+
+
+
+} catch (error) {
+  
+}
+
+
+})
+
