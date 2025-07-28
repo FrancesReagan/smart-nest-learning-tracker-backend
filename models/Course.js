29d9e0cf0ee9df1eb5d39f49f4e6979f8df5bec3
Frances Reagan James <adtgwen@gmail.com>
2025-07-28 11:52:06 -0400

    finished Course Schema

--- models/Course.js
+++ models/Course.js
@@ -17,4 +17,22 @@ const courseSchema = new Schema({
     enum: ["Programming", "Design", "Business", "Data Science", "Investing","Other"],
     default: "Other",
   },
-})
+  url: {
+    type: String,
+    trim: true,
+  },
+  status: {
+    type: String,
+    enum: ["On the horizon","Working it", "BAM did it"],
+    default: "On the horizon",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref:"User",
+    required: true,
+  }
+});
