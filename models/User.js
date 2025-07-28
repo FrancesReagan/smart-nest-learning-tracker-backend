bc2c390389f288a462685abe6f4195b23f46d329
Frances Reagan James <adtgwen@gmail.com>
2025-07-28 12:00:34 -0400

    started adding User Schema

---
+++ models/User.js
@@ -0,0 +1,25 @@
+import mongoose, { Schema } from "mongoose";
+// import bcrypt from "bcrypt";
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, "Must use a valid email address"],
+  },
+  password: {
+    type: String,
+    minlength: 5,
+    required: true
+  }
+});
+
+// hash user password before saving//
+userSchema.
